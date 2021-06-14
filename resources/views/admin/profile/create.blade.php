18ad9925e6e635891720065adfa636b70bc08471
koyama_yuka <y.minimountain.sheep.27@gmail.com>
2021-06-14 03:05:50 +0000

    kadai_10

---
+++ resources/views/admin/profile/create.blade.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset = "utf-8">
+        <meta http-equiv = "X-UA-Compatible" content = "IE=edge">
+        <meta name = "viewport" content = "width=device-width, initial-scale=1">
+        <title>create_test</title>
+    </head>
+    <bpdy>
+        <h1>create_test</h1>
+    </bpdy>
+    
+</html>
