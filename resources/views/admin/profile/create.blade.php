4d4f199a2f2248bce98f1dba95a2a5b38e15abb0
koyama_yuka <y.minimountain.sheep.27@gmail.com>
2021-06-15 08:48:59 +0000

    kadai_11

--- resources/views/admin/profile/create.blade.php
+++ resources/views/admin/profile/create.blade.php
@@ -1,13 +1,16 @@
-<!DOCTYPE html>
-<html>
-    <head>
-        <meta charset = "utf-8">
-        <meta http-equiv = "X-UA-Compatible" content = "IE=edge">
-        <meta name = "viewport" content = "width=device-width, initial-scale=1">
-        <title>create_test</title>
-    </head>
-    <bpdy>
-        <h1>create_test</h1>
-    </bpdy>
-    
-</html>
+{{-- layouts/profile.blade.phpを読み込む --}}
+@extends('layouts.profile')
+
+{{-- profile.blade.phpの@yield('title')に'プロフィール'を埋め込む --}}
+@section('title', 'プロフィール')
+
+{{-- profile.blade.phpの@yield('content')に以下のタグを埋め込む --}}
+@section('content')
+    <div class = "container">
+        <div class = "rom">
+            <div class = "col-md-8 mx-auto">
+                <h2>My プロフィール</h2>
+            </div>
+        </div>
+    </div>
+@endsection
